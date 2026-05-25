f3195b8236e9634a2a696060a1cd91248d55d20d
DayseKs <daysek09@gmail.com>
2026-05-25 12:20:51 -0300

    add

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.use("/books", booksRoutes);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/pages/index.html"));
+});
+
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000");
 });
